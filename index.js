1968bfdf3e80daecbd7530c3802d9b7f39a342e8
NAJEEBKHAN123 <najeebkhanlaku.com>
2024-09-19 10:12:35 +0500

    commit with hello world

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const http = require("http");
+const http = require("http")
 
 const server = http.createServer((req, res) =>{
-    res.end("hello this is server")
+  res.end("helloo")
 })
-
-server.listen(3000, () =>{
-    console.log("server was run on port 3000")
-})
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
